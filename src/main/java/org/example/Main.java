502512f27dbdeb27526f0af705db09e46eb66b04
Giovanni6722 <giodude.dev672@gmail.com>
2026-01-30 17:08:51 -0800

    finished lab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Vehicle v = new Vehicle(2, "Blue", 1.2f, "Gasoline");
+        System.out.println("Vehicle:");
+        System.out.println("Wheels: " + v.getNumberOfWheels());
+        System.out.println("Color: " + v.getColor());
+        System.out.println("Engine: " + v.getEngineSize());
+        System.out.println("Fuel: " + v.getFuelType());
+        System.out.println();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Car c = new Car(4, "Red", 2.5f, "Gasoline", "Toyota");
+        System.out.println("Car:");
+        c.honk();
+        c.displayInfo();
+
+        c.setColor("Black");
+        System.out.println("\nAfter repaint:");
+        c.displayInfo();
     }
 }
